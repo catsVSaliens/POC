46fa27994cd12dee6c0e42ede3add2d2f19f5e8c
TheToled <ittay.toledo@gmail.com>
2015-11-07 16:55:53 +0200

    First Commit!!!

--- Main/src/poc/core/Main.java
+++ Main/src/poc/core/Main.java
@@ -6,6 +6,6 @@ package poc.core;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, it's Tol :)!!!");
+		System.out.println("Hello World, it's Tol :)!!!!");
 	}
 }
